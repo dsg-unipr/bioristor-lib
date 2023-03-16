207ab1f7a0df7096a6450c89a95d233e3c30d5bc
Francesco Saccani <francesco.saccani@unipr.it>
2023-03-16 14:39:12 +0000

    Implement `BruteForceEquation` algorithm

--- bioristor-lib/src/algorithms/brute_force.rs
+++ bioristor-lib/src/algorithms/brute_force.rs
@@ -1,7 +1,7 @@
 use crate::{
     algorithms::Algorithm,
     losses::Loss,
-    models::{Model, SystemModel},
+    models::{EquationModel, Model, SystemModel},
     params::Variables,
     utils::FloatRange,
 };
@@ -20,14 +20,84 @@ pub struct BruteForceParams {
     pub saturation_range: FloatRange,
 }
 
+/// Implementation of the brute force algorithm for the equation model.
+///
+/// # Type parameters
+///
+/// * `M` - The model to be solved.
+/// * `L` - The loss function to be used.
+pub struct BruteForceEquation<M: Model, L: Loss> {
+    /// The parameters of the algorithm.
+    params: BruteForceParams,
+
+    /// The model to be solved.
+    model: M,
+
+    _t: core::marker::PhantomData<L>,
+}
+
+impl<M, L> Algorithm<BruteForceParams, M> for BruteForceEquation<M, L>
+where
+    M: EquationModel,
+    L: Loss<ModelOutput = f32>,
+{
+    /// Create a new instance of the brute force algorithm.
+    ///
+    /// # Arguments
+    ///
+    /// * `params` - The parameters of the algorithm.
+    /// * `model` - The model to be solved by the algorithm.
+    fn new(params: BruteForceParams, model: M) -> Self {
+        Self {
+            params,
+            model,
+            _t: core::marker::PhantomData,
+        }
+    }
+
+    /// Tries to solve the model for the given parameters using the brute force
+    /// algorithm and returns the best solution found.
+    ///
+    /// # Returns
+    ///
+    /// * `Some((vars, loss))` - The variables and the loss of the solution.
+    /// * `None` - If the algorithm could not find a solution.
+    fn run(&self) -> Option<(Variables, f32)> {
+        let mut best: Option<(f32, f32)> = None;
+
+        for concentration in self.params.concentration_range.clone() {
+            let error = L::evaluate(self.model.value(concentration));
+
+            match best {
+                Some((_, best_error)) if error < best_error => {
+                    best = Some((concentration, error));
+                }
+                None => {
+                    best = Some((concentration, error));
+                }
+                _ => (),
+            }
+        }
+
+        best.map(|(concentration, error)| {
+            (
+                Variables {
+                    concentration,
+                    resistance: self.model.resistance(concentration),
+                    saturation: self.model.saturation(concentration),
+                },
+                error,
+            )
+        })
+    }
+}
+
 /// Implementation of the brute force algorithm for the system model.
 ///
 /// # Type parameters
 ///
 /// * `M` - The type of the model.
 /// * `L` - The type of the loss.
-#[derive(Debug, PartialEq, Clone)]
-#[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub struct BruteForceSystem<M: Model, L: Loss> {
     /// The parameters of the algorithm.
     params: BruteForceParams,
@@ -96,12 +166,46 @@ where
 #[cfg(test)]
 mod tests {
     use crate::{
-        losses::SumRelative,
+        losses::{Absolute, SumRelative},
         models::{Model, SystemModel},
         params::{Currents, ModelParams},
     };
 
     use super::*;
+
+    struct EquationModelMock;
+
+    impl Model for EquationModelMock {
+        fn new(_: ModelParams, _: Currents) -> Self {
+            Self
+        }
+
+        fn params(&self) -> &ModelParams {
+            unimplemented!()
+        }
+
+        fn currents(&self) -> &Currents {
+            unimplemented!()
+        }
+    }
+
+    impl EquationModel for EquationModelMock {
+        fn value(&self, concentration: f32) -> f32 {
+            (concentration - 2.0).powi(2)
+        }
+
+        fn gradient(&self, concentration: f32) -> f32 {
+            2.0 * (concentration - 2.0)
+        }
+
+        fn resistance(&self, concentration: f32) -> f32 {
+            concentration
+        }
+
+        fn saturation(&self, concentration: f32) -> f32 {
+            concentration
+        }
+    }
 
     struct SystemModelMock;
 
@@ -134,7 +238,25 @@ mod tests {
     }
 
     #[test]
-    fn test_brute_force() {
+    fn test_brute_force_equation() {
+        let params = BruteForceParams {
+            concentration_range: FloatRange::new(0.0, 10.0, 10),
+            resistance_range: FloatRange::new(0.0, 1.0, 10),
+            saturation_range: FloatRange::new(0.0, 1.0, 10),
+        };
+        let model = EquationModelMock;
+
+        let algorithm = BruteForceEquation::<_, Absolute>::new(params, model);
+        let (vars, error) = algorithm.run().unwrap();
+
+        assert!((vars.concentration - 2.0).abs() < 1e-6);
+        assert!((vars.resistance - 2.0).abs() < 1e-6);
+        assert!((vars.saturation - 2.0).abs() < 1e-6);
+        assert!(error.abs() < 1e-6);
+    }
+
+    #[test]
+    fn test_brute_force_system() {
         let params = BruteForceParams {
             concentration_range: FloatRange::new(0.0, 1.0, 10),
             resistance_range: FloatRange::new(0.0, 1.0, 10),

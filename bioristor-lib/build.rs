e3f9bfa59bf4de7ea363ff45839790cb79267f24
Francesco Saccani <francesco.saccani@unipr.it>
2023-04-18 09:19:38 +0000

    Remove unused features

--- bioristor-lib/build.rs
+++
@@ -1,7 +0,0 @@
-#[cfg(not(any(feature = "equation", feature = "system")))]
-compile_error!("you have to select exactly one feature between `equation` and `system`");
-
-#[cfg(all(feature = "equation", feature = "system"))]
-compile_error!("features `equation` and `system` are mutually exclusive");
-
-fn main() {}
